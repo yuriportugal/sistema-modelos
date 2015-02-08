4fd652ebbf5ce94a00de3c8f8b7fe0e566d538b6
y.portugal@pucp.pe <y.portugal@pucp.pe@3876642b-4cc9-9523-e4bc-dd63ea70fca8>
2015-02-08 04:56:53 +0000

    Carga automatica de balance inicial para un modelo nuevo.

--- SISTEMA_MODELOS/src/main/java/sistema/view/managedbean/util/RedirectionerController.java
+++ SISTEMA_MODELOS/src/main/java/sistema/view/managedbean/util/RedirectionerController.java
@@ -4,8 +4,12 @@
  */
 package sistema.view.managedbean.util;
 
+import java.math.BigDecimal;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
+import sistema.modelos.vista.controlador.modelo.ModeloControlador;
+import sistema.modelos.vista.controlador.util.UsuarioControlador;
 
 /**
  *
@@ -14,7 +18,29 @@ import javax.faces.bean.RequestScoped;
 @ManagedBean(name = "redirectionerController")
 @RequestScoped
 public class RedirectionerController {
+    
+    @ManagedProperty(value="#{usuarioControlador}")
+    private UsuarioControlador usuarioControlador;
+    
+    @ManagedProperty(value="#{modeloControlador}")
+    private ModeloControlador modeloControlador;
+    
+    
+    public UsuarioControlador getUsuarioControlador() {
+        return usuarioControlador;
+    }
 
+    public void setUsuarioControlador(UsuarioControlador usuarioControlador) {
+        this.usuarioControlador = usuarioControlador;
+    }
+
+    public ModeloControlador getModeloControlador() {
+        return modeloControlador;
+    }
+
+    public void setModeloControlador(ModeloControlador modeloControlador) {
+        this.modeloControlador = modeloControlador;
+    }
     
     public RedirectionerController() {
     }
@@ -43,6 +69,54 @@ public class RedirectionerController {
     }
     
      public String crearModelo(){
+         
+     getModeloControlador().getCurrentModelo().setACCajaBanco(new BigDecimal(getUsuarioControlador().getCurrentUsuario().getEmpresa().getBiCajaBancos()));
+     getModeloControlador().getCurrentModelo().setACCredFiscal(new BigDecimal(getUsuarioControlador().getCurrentUsuario().getEmpresa().getBiCreditoFiscal()));
+     getModeloControlador().getCurrentModelo().setACCuentaxCobrar(new BigDecimal(getUsuarioControlador().getCurrentUsuario().getEmpresa().getBiCuentasXCobrar()));
+     getModeloControlador().getCurrentModelo().setACInsumos(new BigDecimal(getUsuarioControlador().getCurrentUsuario().getEmpresa().getBiExistInsumos()));
+     getModeloControlador().getCurrentModelo().setACProductosTerminados(new BigDecimal(getUsuarioControlador().getCurrentUsuario().getEmpresa().getBiExistProdTerminados()));
+     getModeloControlador().getCurrentModelo().setANCActivoFijo(new BigDecimal(getUsuarioControlador().getCurrentUsuario().getEmpresa().getBiActivosFijos()));
+     getModeloControlador().getCurrentModelo().setANCAmortAcumulada(new BigDecimal(getUsuarioControlador().getCurrentUsuario().getEmpresa().getBiAmortAcumulada()));
+     getModeloControlador().getCurrentModelo().setANCDepreciacionAcum(new BigDecimal(getUsuarioControlador().getCurrentUsuario().getEmpresa().getBiDeprecAcumulada()));
+     getModeloControlador().getCurrentModelo().setANCIntangible(new BigDecimal(getUsuarioControlador().getCurrentUsuario().getEmpresa().getBiIntangibles()));
+     getModeloControlador().getCurrentModelo().setANCValNegociable(new BigDecimal(getUsuarioControlador().getCurrentUsuario().getEmpresa().getBiValoresNegociables()));
+     getModeloControlador().getCurrentModelo().setPCCuentaxCobrarProveedores(new BigDecimal(getUsuarioControlador().getCurrentUsuario().getEmpresa().getBiCuentasXPagarProv()));
+     getModeloControlador().getCurrentModelo().setPCCuentaxCobrarSobregiro(new BigDecimal(getUsuarioControlador().getCurrentUsuario().getEmpresa().getBiCuentasxPagarSobregiro()));
+     getModeloControlador().getCurrentModelo().setPCDirBuenaGest(new BigDecimal(getUsuarioControlador().getCurrentUsuario().getEmpresa().getBiDirecBuenaGestion()));
+     getModeloControlador().getCurrentModelo().setPCDividendoxPag(new BigDecimal(getUsuarioControlador().getCurrentUsuario().getEmpresa().getBiDividendos()));
+     getModeloControlador().getCurrentModelo().setPCIGVxPagar(new BigDecimal(getUsuarioControlador().getCurrentUsuario().getEmpresa().getBiIGV()));
+     getModeloControlador().getCurrentModelo().setPCImpuestoRenta(new BigDecimal(getUsuarioControlador().getCurrentUsuario().getEmpresa().getBiImpuestoRenta()));
+     getModeloControlador().getCurrentModelo().setPCPartixPagar(new BigDecimal(getUsuarioControlador().getCurrentUsuario().getEmpresa().getBiParticipaciones()));
+     getModeloControlador().getCurrentModelo().setPCPrestxPagarCortoPlazo(new BigDecimal(getUsuarioControlador().getCurrentUsuario().getEmpresa().getBiPrestamosCortoPlazo()));
+     getModeloControlador().getCurrentModelo().setPNCDeudaLargoPlazo(new BigDecimal(getUsuarioControlador().getCurrentUsuario().getEmpresa().getBiDeudasLargoPlazo()));
+     getModeloControlador().getCurrentModelo().setPATCapitalSocial(new BigDecimal(getUsuarioControlador().getCurrentUsuario().getEmpresa().getBiCapitalSocial()));
+     getModeloControlador().getCurrentModelo().setPATReservaLegal(new BigDecimal(getUsuarioControlador().getCurrentUsuario().getEmpresa().getBiReservaLegalAcum()));
+     getModeloControlador().getCurrentModelo().setPATResultadoAcumulado(new BigDecimal(getUsuarioControlador().getCurrentUsuario().getEmpresa().getBiResultAcumulados()));
+     
+     
+                     
+           
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+         
         return "/MODELO/CREARMODELO";
     }
     public String listarModelo(){
